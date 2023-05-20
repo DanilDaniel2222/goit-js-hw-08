a05d37a74d8d27b1f9bcf249a73b209c5f2bf566
Danil Kuskalo <danilkuskalo1999@gmail.com>
2023-05-20 10:08:40 +0300

    new commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
-// Ознайомся з документацією бібліотеки Vimeo плеєра.
-// Додай бібліотеку як залежність проекту через npm.
-// Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
-// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
-// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
-// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
-// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+// 1. Ознайомся з документацією бібліотеки Vimeo плеєра.
+// 2. Додай бібліотеку як залежність проекту через npm.
+// 3. Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+// 4. Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// 5. Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// 6. Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+// 7. Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
 
 
 
@@ -14,12 +14,16 @@ import throttle from 'lodash/throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
 
 player.on('timeupdate', throttle(onPause, 1000));
-
 player.setCurrentTime(currentTime);
 
-function onPause({ seconds } = 0){
-    localStorage.setItem('videoplayer-current-time', seconds);
+function onPause(data){
+    const timeToWrite = JSON.stringify(data.seconds);
+    localStorage.setItem('videoplayer-current-time', timeToWrite);
 }
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+// 1. Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
+// 2. Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// 3. Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// 4. Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
 
 
 import throttle from 'lodash/throttle';
@@ -11,36 +11,64 @@ const refs = {
     form: document.querySelector('form'),
     input: document.querySelector('input'),
     textarea: document.querySelector('textarea'),
-}
+};
 
-const SAVE_FORM = "feedback-form-state";
-const dataForm = {};
+const data = JSON.parse(localStorage.getItem('feedback-form-state')) || {          
+    email: '',
+    message: '',
+};
 
+refs.input.value = data.email;
+refs.textarea.value = data.message;
+
+refs.form.addEventListener("input", throttle(onInput, 500));
 refs.form.addEventListener('submit', onSubmit);
-refs.form.addEventListener('input', throttle(onInput, 500));
-
-fillTextarea();
-
-function fillTextarea(){
-    const savedText = JSON.parse(localStorage.getItem('SAVE_FORM')) || {}
-    if(savedText.email){
-        email.value = savedText.email
-    }
-    if(savedText.message){
-        textarea.value = savedText.message
-    }
-}
 
 function onInput(event){
-    dataForm[event.target.name] = event.target.value;
-    localStorage.setItem('LOCAL_STORAGE_KEY', JSON.stringify(dataForm));
+    if(event.target.nodeName === 'INPUT'){
+        data.email = event.target.value;
+    }
+
+    if(event.target.nodeName === 'TEXTAREA'){
+        data.message = event.target.value;
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
 
 function onSubmit(event){
     event.preventDefault();
+
+    const {
+        elements: {email, message},
+    } = event.currentTarget;
+
+    const userInfo = {
+        email: email.value,
+        message: message.value,
+    };
+
+    console.log(userInfo);
     event.currentTarget.reset();
-    localStorage.removeItem('SAVE_FORM');
-    console.log(dataForm);
+    localStorage.removeItem('feedback-form-state');
 }
 
-console.log(hello); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
